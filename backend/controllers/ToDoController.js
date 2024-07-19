bd5820a2ea9f82c31d328566bb4b6b844a185d2d
CodingbyDaniel <109753547+Dana-Codes@users.noreply.github.com>
2024-07-19 20:12:50 +0100

    first push

---
+++ backend/controllers/ToDoController.js
@@ -0,0 +1,30 @@
+const ToDoModel = require("../models/ToDoModel");
+
+module.exports.getToDo = async (req, res) => {
+  const Todo = await ToDoModel.find();
+  res.send(toDo);
+};
+
+module.exports.saveTodo = async (req, res) => {
+  const { text } = req.body;
+
+  ToDoModel.create({ text }).then((data) => {
+    console.log("Added Successfully");
+    console.log(data);
+    res.send(data);
+  });
+};
+
+module.exports.updateToDo = async (req, res) => {
+	const { id, text } = req.body;
+	ToDoModel.findByIdAndUpdate(_id, { text })
+	  .then(() => res.send("Update Succesfully"))
+	  .catch((err) => console.log(err));
+  };
+  
+  module.exports.deleteToDo = async (req, res) => {
+	const { id, text } = req.body;
+	ToDoModel.findByIdAndDelete(_id)
+	  .then(() => res.send("Deleted Succesfully"))
+	  .catch((err) => console.log(err));
+  };
